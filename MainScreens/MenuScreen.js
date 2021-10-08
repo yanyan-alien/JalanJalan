998f1ac2233bd57289a5310b2571bb6cc13ab186
yanyan-alien <linyanqi@gmail.com>
2021-10-09 02:56:40 +0800

    Separated Main Screens from App.js

---
+++ MainScreens/MenuScreen.js
@@ -0,0 +1,60 @@
+// import { createStackNavigator } from "@react-navigation/stack";
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+
+export default function HomeScreen() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.feature_box}>
+        <TouchableOpacity style={{ flex: 1, backgroundColor: '#F8FFA5' , justifyContent:"center"}}> 
+              <Text style={{textAlign:"center"}}>TAXI</Text>
+              <Image style={styles.pictures} source={require('../assets/local_taxi.png')}></Image>
+          </TouchableOpacity>
+          
+          <TouchableOpacity style={{ flex: 1, backgroundColor: '#A5EFFF' , justifyContent:"center"}} >
+              <Text style={{textAlign:"center"}}>HEALTH</Text>
+              <Image style={styles.pictures} source={require('../assets/health.png')}></Image>
+          </TouchableOpacity>
+        </View>
+          
+        <View style={styles.feature_box}>
+          <TouchableOpacity
+             style={{ flex: 1, backgroundColor: '#B1F698' , justifyContent:"center"}}>
+              <Text style={{textAlign:"center"}}>FOOD</Text>  
+              <Image style={styles.pictures} source={require('../assets/restaurant.png')}></Image>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{ flex: 1, backgroundColor: '#FF9292' , justifyContent:"center"}} >
+              <Text style={{textAlign:"center"}}>EMERGENCY</Text>  
+              <Image style={styles.pictures} source={require('../assets/emergency.png')}></Image>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      // flexDirection: 'column',
+      flex: 2,
+      // paddingTop: 20,
+      backgroundColor: "white",
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    feature_box: {
+      flex: 1,
+      flexDirection: "row",
+      alignContent: "flex-end",
+    },
+    pictures: {
+      height:"30%",
+      width: "100%",
+      alignSelf: "center",
+      resizeMode: "contain",
+    },
+    rectanges: {
+      flex: 1,
+      justifyContent: "center"
+    }
+  });
