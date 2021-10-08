21d6b0969212cf804f350b99a3e52c7f095b66cd
christoarif <casetiadharma@gmail.com>
2021-10-08 10:43:29 +0800

    Added FoodGUI

--- App.js
+++ App.js
@@ -134,11 +134,130 @@ const TaxiScreen = ({navigation}) =>{
     </View>
   );
 }
-
 const FoodScreen = ({navigation}) =>{
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Food Things!</Text>
+  <View style = {[styles.container, {flexDirection:"column"}]}>
+
+    <View style={styles.feature_box}>
+        <TouchableOpacity style={{ 
+          backgroundColor: '#B1F698' , 
+          justifyContent:"center",
+          width:350,
+          borderRadius: 24,
+          top:"20%",
+          }}
+          onPress ={() => navigation.navigate('FoodReco')}>
+        <Text style={{textAlign:"center", fontSize:30}}>Coffeeshop/Hawker Centre</Text>
+        </TouchableOpacity>
+    </View>
+
+    <View style={{ flex:1, justifyContent: 'center', alignItems: 'center'}}>
+      <View style={styles.feature_box}>
+        <TouchableOpacity style={{
+          backgroundColor: '#B1F698' , 
+          justifyContent:"center",
+          width:350,
+          borderRadius: 24,
+          top:"40%"
+          }}
+          onPress ={() => navigation.navigate('FoodReco')}>
+        <Text style={{textAlign:"center", fontSize:30}}>Restaurants</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+    <View style ={{flex:1}}></View>
+  </View>
+  );
+}
+
+const FoodRecoScreen = ({navigation}) =>{
+  return (
+    <View style = {[styles.container, {flexDirection:"column"}]}>  
+      <View style={{ flex:2, justifyContent: 'center', alignItems: 'center'}}>
+          <Text style = 
+          {{fontSize : 30,
+            marginTop:"10%"}}>
+            1. (Choice 1) 
+          </Text>
+      </View>
+  
+      <View style={styles.feature_box}>
+          <TouchableOpacity style={{ 
+            backgroundColor: '#B1F698' , 
+            justifyContent:"center",
+            width:350,
+            borderRadius: 24}}
+            onPress ={() => navigation.navigate('FoodList')}>
+          <Text style={{textAlign:"center", fontSize:30}}>Directions</Text>
+          </TouchableOpacity>
+      </View>
+     
+  
+      <View style={{ flex:2, justifyContent: 'center', alignItems: 'center'}}>
+          <Text style = 
+          {{fontSize : 30, 
+            marginTop:"10%"}}>
+            2. (Choice 2) 
+          </Text>
+      </View>
+  
+      <View style={{ flex:1, justifyContent: 'center', alignItems: 'center'}}>
+        <View style={styles.feature_box}>
+          <TouchableOpacity style={{
+            backgroundColor: '#B1F698' , 
+            justifyContent:"center",
+            width:350,
+            borderRadius: 24,}}
+            onPress ={() => navigation.navigate('FoodList')}>
+          <Text style={{textAlign:"center", fontSize:30}}>Directions</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View style ={{flex:1}}></View>
+    </View>
+    );
+}
+
+const Food_listScreen = ({navigation}) =>{
+  return (
+      <View style={{ flex:1, justifyContent: 'center', alignItems: 'center'}}>
+        <View style={styles.feature_box}>
+          <TouchableOpacity style={{ 
+          backgroundColor: '#B1F698' , 
+          justifyContent:"center",
+          width:1000,
+          borderRadius: 24}}
+          onPress ={() => navigation.navigate('FoodMap')}>
+          <Text style={{textAlign:"center", fontSize:30}}>Tap for Map View</Text>
+          </TouchableOpacity>
+        </View>
+      <View style={{ flex:10, justifyContent: 'center', alignItems: 'center'}}>
+        <Text style = 
+        {{fontSize : 30, 
+          marginTop:"10%"}}>
+          (Directions by Google Maps) 
+        </Text>
+    </View>
+    </View>
+  );
+}
+
+const Food_MapScreen = ({navigation}) =>{
+  return (
+      <View style={{ flex:1, justifyContent: 'center', alignItems: 'center'}}>
+        <View style={styles.feature_box}>
+          <TouchableOpacity style={{ 
+          backgroundColor: '#B1F698' , 
+          justifyContent:"center",
+          width:1000,
+          borderRadius: 24}}
+          onPress ={() => navigation.navigate('FoodList')}>
+          <Text style={{textAlign:"center", fontSize:30}}>Tap for List View</Text>
+          </TouchableOpacity>
+        </View>
+      <View style={{ flex:10, justifyContent: 'center', alignItems: 'center'}}>
+        <Text>Put Image here</Text>
+      </View>
     </View>
   );
 }
@@ -165,13 +284,16 @@ function Home(){
       <Stack.Navigator
       screenOptions ={{headerShown: false}}>
         <Stack.Screen name="HomeScreen" component={HomeScreen}/>
-        <Stack.Screen name= "Health" component={HealthScreen} />
+        <Stack.Screen name= "Health" component={HealthScreen}/>
         <Stack.Screen name = "Taxi" component={TaxiScreen}/>
-        <Stack.Screen name = "Food" component={FoodScreen}/>
+        <Stack.Screen name = "Food"  component={FoodScreen}/>
         <Stack.Screen name = "Emergency" component={EmergencyScreen}/>
         <Stack.Screen name = "HealthList" component={Health_listScreen}/>
         <Stack.Screen name = "HealthMap" component={Health_MapScreen}/>
         <Stack.Screen name = "Reminder" component ={ReminderScreen}/>
+        <Stack.Screen name = "FoodReco" component={FoodRecoScreen}/>
+        <Stack.Screen name = "FoodList" component={Food_listScreen}/>
+        <Stack.Screen name = "FoodMap" component={Food_MapScreen}/>
       </Stack.Navigator>
   )
   
